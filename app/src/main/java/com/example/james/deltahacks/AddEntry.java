0236b0a67078329273ae57bd1b55391ee8ed4ab8
James <jameslezadabonifacio@gmail.com>
2017-01-28 23:09:18 -0500

    added more stuff

--- app/src/main/java/com/example/james/deltahacks/AddEntry.java
+++ app/src/main/java/com/example/james/deltahacks/AddEntry.java
@@ -2,6 +2,12 @@ package com.example.james.deltahacks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class AddEntry extends AppCompatActivity {
 
@@ -10,6 +16,20 @@ public class AddEntry extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_entry);
 
-        String foods[] = {};
+        String foods[] = {"Beans", "Greens", "Potatoes", "Tomatoes", "Lamb", "Rams", "Hogs", "Dogs"};
+
+        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, foods);
+        ListView listView = (ListView)findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        String food = String.valueOf(parent.getItemAtPosition((position)));
+                        Toast.makeText(AddEntry.this, food, Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
     }
 }
