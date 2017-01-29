e46b0a1c1dd26bfc814d6228c03dcf0aa9e538b8
James <jameslezadabonifacio@gmail.com>
2017-01-29 11:01:29 -0500

    yo

--- app/src/main/java/com/example/james/deltahacks/InitialSetup.java
+++ app/src/main/java/com/example/james/deltahacks/InitialSetup.java
@@ -1,20 +1,34 @@
 package com.example.james.deltahacks;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
 public class InitialSetup extends AppCompatActivity {
     private RadioButton radioCustom;
     private EditText field;
+    private Button btnNext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_setup);
+
+        btnNext = (Button)findViewById(R.id.btnNext);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), MainActivity.class);
+
+                startActivity(i);
+            }
+        });
 
 
         radioCustom = (RadioButton)findViewById(R.id.radioCustom);
