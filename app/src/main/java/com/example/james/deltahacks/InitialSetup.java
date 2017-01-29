5994b08ae4ebde84bffd8cf3788631c6f95a2bdd
loicmurumba <muruloic@gmail.com>
2017-01-29 04:34:28 -0500

    initial page

--- app/src/main/java/com/example/james/deltahacks/InitialSetup.java
+++ app/src/main/java/com/example/james/deltahacks/InitialSetup.java
@@ -1,13 +1,30 @@
 package com.example.james.deltahacks;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class InitialSetup extends AppCompatActivity {
-
+    private RadioButton custom;
+    private EditText field;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_setup);
+
+        custom = (RadioButton)findViewById(R.id.customm);
+        field = (EditText)findViewById(R.id.dockey);
+
+        View.OnClickListener listen = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // field.setHintTextColo;
+            }
+        };
+
+
     }
 }
